b601fcc1e15aff53547b0e3b1cbf61baa00f0618
siguoya <924714558@qq.com>
2018-09-14 17:17:04 +0800

    site sucker

---
+++ Mid-Autumn/news.sohu.com/upload/commonzzx/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?c154d502f02b12a996c34a02281a3bff";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
